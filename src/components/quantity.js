9fd5714e67c53272563ce77afbd3bf54d2cab6d0
kamryn0906 <delaleu.k@gmail.com>
2021-01-29 11:24:21 -0500

    buttons

--- src/components/quantity.js
+++ src/components/quantity.js
@@ -2,18 +2,18 @@ import React, { Component } from 'react';
 
 class Quantity extends Component {
     render() {
-        const { className, quantity } = this.props;
+        const { className, quantity, icon, onClick } = this.props;
         return (
             <div className={`${className} quantity`}>
                 <div className='quantity__count'>
                     {quantity}
                 </div>
-                <div className='quantity__plus'>
-                    <i className='fas fa-plus'></i>
-                </div>
-                <div className='quantity__minus'>
-                    <i className='fas fa-minus'></i>
-                </div>
+                <a onClick={this.handleAddToCart} className={`${className} quantity__plus`}>
+                    <i className='fas fa-plus'></i>     
+                </a>
+                <a onClick={this.handleAddToCart} className={`${className} quantity__minus`}>
+                    <i className='fas fa-minus'></i>      
+                </a>
             </div>
         )
     }
--- src/components/shop/cartProduct.js
+++ src/components/shop/cartProduct.js
@@ -5,14 +5,16 @@ import PriceTag from '../priceTag';
 class CartProduct extends Component {
     render() {
         console.log(this.props);
-        const { className, product, quantity } = this.props;
+        const { className, product, quantity, icon, onClick } = this.props;
         const { title, price } = product;
         return (
             <div className={`${className} cart-product`}>
                 <img className='cart-product__image' src='http://via.placeholder.com/130x130'/>
                 <div className='cart-product__title'>{title}</div>
                 <Quantity className='cart-product__quantity' quantity={quantity}/>
-                <a className='cart-product__remove'>Remove</a>
+                <a onClick={this.handleAddToCart} className={`${className} cart-product__remove`}>
+                    <i class="fas fa-trash"></i>      
+                </a>
                 <PriceTag className='cart-product__price' title={price}/>
             </div>
         )
